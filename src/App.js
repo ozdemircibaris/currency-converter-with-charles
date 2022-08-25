8d9d7baae1d580add1ca58b3b980f81b457c5895
Baris Ozdemirci <42464623+ozdemircibaris@users.noreply.github.com>
2022-08-26 00:57:52 +0300

    app building

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { getCurrenciesRequest } from "./services/currencies";
 
 function App() {
+  // states
+  const [selectedCurrency, setSelectedCurrency] = useState("fruit");
+  const [currencies, setCurrencies] = useState({});
+
+  const getData = async () => {
+    const currencyList = await getCurrenciesRequest();
+    console.log("currencyList", currencyList);
+    setCurrencies(currencyList.rates);
+  };
+
+  const onCurrencyChange = (event) => {
+    setSelectedCurrency(event.target.value);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label>
+        Currencies
+        <select value={selectedCurrency} onChange={onCurrencyChange}>
+          {Object.keys(currencies)?.map((currency) => {
+            return (
+              <option key={currency} value={currency}>
+                {currency}
+              </option>
+            );
+          })}
+        </select>
+      </label>
+      {/* second select option */}
+      <label>
+        Currency amont
+        <select value={selectedCurrency} onChange={onCurrencyChange}>
+          <option key="USD" value="0.997655"></option>
+          <option key="CAD" value="1.290417"></option>
+        </select>
+      </label>
     </div>
   );
 }
